f01cc96748b2d5d5cfe06ef082f9cda6c433a4f0
hyuna-kang <kho8815@gamil.com>
2025-07-23 12:46:23 +0900

    feat: project 관련 엔티티 생성 (project, active instance)
    - image, user 엔티티 임의 생성

---
+++ src/main/java/com/growlog/webide/domain/images/entity/Image.java
@@ -0,0 +1,23 @@
+package com.growlog.webide.domain.images.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Image {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String name;
+}
---
+++ src/main/java/com/growlog/webide/domain/users/entity/User.java
@@ -0,0 +1,25 @@
+package com.growlog.webide.domain.users.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users") // USER는 DB 예약어일 수 있으므로 "users"로 지정
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String username;
+}
