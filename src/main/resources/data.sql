5eed02cf6bd7013fbc8a69d496764d97926e07e5
yujinkim <yujin9600@gmail.com>
2025-07-30 11:45:35 +0900

    test: 유저 더미 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,9 +5,13 @@ VALUES ('test_user', 'test@test.com', '$2a$12$wYwlA/Kof2KH2sJrbNaLXe4qpaux.LidOE
         CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
        ('test_user2', 'test2@test.com', '$2a$12$wYwlA/Kof2KH2sJrbNaLXe4qpaux.LidOEHZnLrM8boW7IyZSiHra',
         'https://kr.freepik.com/free-photo/beautiful-view-sunset-sea_3624503.htm#from_element=photos_discover&from_view=subhome',
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       ('test_user3', 'test3@test.com', '$2a$12$Itna5eUdS9i91YLx2jTGkuyWRECaJqJHSAWKFLnQkaK6FFzewIZ2i',
+        'https://kr.freepik.com/free-photo/beautiful-view-sunset-sea_3624503.htm#from_element=photos_discover&from_view=subhome',
         CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 -- 초기 개발 환경 이미지 데이터 삽입
-INSERT INTO images (image_id, image_name, version, docker_base_image, build_command, run_command, template_code)
+INSERT
+INTO images (image_id, image_name, version, docker_base_image, build_command, run_command, template_code)
 VALUES (1, 'java', '17', 'openjdk:17-jdk-slim', 'javac Main.java', 'java Main',
         'public class Main {\n    public static void main(String[] args) {\n        System.out.println("Hello, Java 17 World!");\n    }\n}');
 -- 초기 프로젝트 데이터 삽입
