c1444c919d44ed82ec506b3281edad09a467000a
yujinkim <yujin9600@gmail.com>
2025-07-31 16:35:03 +0900

    feat: 프로젝트 트리 관련 웹소켓 응답 dto에 id 추가

--- src/main/java/com/growlog/webide/domain/files/dto/tree/TreeNodeDto.java
+++ src/main/java/com/growlog/webide/domain/files/dto/tree/TreeNodeDto.java
@@ -7,13 +7,19 @@ import lombok.Getter;
 
 @Getter
 public class TreeNodeDto {
+	private Long id;
 	private String path;
 	private String type;
 	private List<TreeNodeDto> children;
 
-	public TreeNodeDto(String path, String type) {
+	public TreeNodeDto(Long id, String path, String type) {
+		this.id = id;
 		this.path = path;
 		this.type = type;
 		this.children = "folder".equals(type) ? new ArrayList<>() : null;
+	}
+
+	public void addChild(TreeNodeDto child) {
+		this.children.add(child);
 	}
 }
