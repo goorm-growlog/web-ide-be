64e1b575ec4ed6fa843e17679aacdad70d65b714
jaeyeopme <jaeyeopme@gmail.com>
2025-08-01 02:18:18 +0900

    fix: s3 config value 경로 수정

--- src/main/java/com/growlog/webide/config/S3Config.java
+++ src/main/java/com/growlog/webide/config/S3Config.java
@@ -13,13 +13,13 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 @Profile({"prod", "s3"})
 @Configuration
 public class S3Config {
-	@Value("${cloud.aws.credentials.access-key}")
+	@Value("${spring.cloud.aws.credentials.access-key}")
 	private String accessKey;
 
-	@Value("${cloud.aws.credentials.secret-key}")
+	@Value("${spring.cloud.aws.credentials.secret-key}")
 	private String secretKey;
 
-	@Value("${cloud.aws.region.static}")
+	@Value("${spring.cloud.aws.region.static}")
 	private String region;
 
 	@Bean
