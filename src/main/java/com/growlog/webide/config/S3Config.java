273bfe1c54913075b805e61234b9d8523a67662c
jaeyeopme <jaeyeopme@gmail.com>
2025-08-02 03:22:07 +0900

    fix: s3 config properties injection 수정

--- src/main/java/com/growlog/webide/config/S3Config.java
+++ src/main/java/com/growlog/webide/config/S3Config.java
@@ -1,26 +1,29 @@
 package com.growlog.webide.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-
 @Profile({"prod"})
 @Configuration
 public class S3Config {
-	@Value("${spring.cloud.aws.credentials.access-key}")
-	private String accessKey;
 
-	@Value("${spring.cloud.aws.credentials.secret-key}")
-	private String secretKey;
+	private final String accessKey;
+	private final String secretKey;
+	private final String region;
 
-	@Value("${spring.cloud.aws.region.static}")
-	private String region;
+	private S3Config(@Value("${spring.cloud.aws.credentials.access-key}") final String accessKey,
+					 @Value("${spring.cloud.aws.credentials.secret-key}") final String secretKey,
+					 @Value("${spring.cloud.aws.region.static}") final String region) {
+		this.accessKey = accessKey;
+		this.secretKey = secretKey;
+		this.region = region;
+	}
 
 	@Bean
 	public AmazonS3 amazonS3() {
