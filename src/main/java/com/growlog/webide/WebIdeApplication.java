c3ed298495e0c20c087667ad95f191e34866ceb2
hyuna-kang <kho8815@gamil.com>
2025-08-14 00:54:47 +0900

    fix: 서버 타임존 UTC -> Asia/Seoul

--- src/main/java/com/growlog/webide/WebIdeApplication.java
+++ src/main/java/com/growlog/webide/WebIdeApplication.java
@@ -1,7 +1,5 @@
 package com.growlog.webide;
 
-import java.util.TimeZone;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -11,7 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class WebIdeApplication {
 
 	public static void main(String[] args) {
-		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(WebIdeApplication.class, args);
 	}
 
