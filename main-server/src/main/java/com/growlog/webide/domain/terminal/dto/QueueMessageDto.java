4e2691267ffddc662768c7bbf1b6cf8eb4e49ebf
yuj118 <yujin9600@naver.com>
2025-08-20 19:21:15 +0900

    feat: 코드 실행 관련 dto 작성

---
+++ main-server/src/main/java/com/growlog/webide/domain/terminal/dto/QueueMessageDto.java
@@ -0,0 +1,15 @@
+package com.growlog.webide.domain.terminal.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class QueueMessageDto {
+	private Long projectId;
+	private String filePath;
+	private Long userId;
+	private String language;
+}
