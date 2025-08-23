4043ff234993e78d14fc332be72717c28aa39513
yuj118 <yujin9600@naver.com>
2025-08-23 15:48:15 +0900

    perf: 터미널 실행과 코드 실행의 분리를 위한 설정 추가

--- worker-server/src/main/java/com/growlog/webide/workers/config/RabbitmqConfig.java
+++ worker-server/src/main/java/com/growlog/webide/workers/config/RabbitmqConfig.java
@@ -15,30 +15,54 @@ import org.springframework.context.annotation.Configuration;
 @EnableRabbit
 public class RabbitmqConfig {
 
-	@Value("${rabbitmq.exchange.name}")
-	private String exchangeName;
-	@Value("${rabbitmq.queue.name}")
-	private String queueName;
-	@Value("${rabbitmq.routing.key}")
-	private String routingKey;
+	// 코드 실행 큐 관련 설정 값
+	@Value("${code-execution.rabbitmq.exchange.name}")
+	private String codeExecutionExchangeName;
+	@Value("${code-execution.rabbitmq.queue.name}")
+	private String codeExecutionQueueName;
+	@Value("${code-execution.rabbitmq.routing.key}")
+	private String codeExecutionRoutingKey;
 
+	// 터미널 명령어 큐 관련 설정 값
+	@Value("${terminal-command.rabbitmq.exchange.name}")
+	private String terminalCommandExchangeName;
+	@Value("${terminal-command.rabbitmq.queue.name}")
+	private String terminalCommandQueueName;
+	@Value("${terminal-command.rabbitmq.routing.key}")
+	private String terminalCommandRoutingKey;
+
+	// 코드 실행 큐, 교환기, 바인딩
 	@Bean
-	public Queue queue() {
-		// 메시지가 쌓일 큐를 정의합니다.
-		// durable: true는 RabbitMQ가 재시작되어도 큐가 유지되도록 설정합니다.
-		return new Queue(queueName, true);
+	public Queue codeExecutionQueue() {
+		return new Queue(codeExecutionQueueName, true);
 	}
 
 	@Bean
-	public TopicExchange exchange() {
-		// 메시지를 라우팅할 교환기를 정의합니다.
-		return new TopicExchange(exchangeName);
+	public TopicExchange codeExecutionExchange() {
+		return new TopicExchange(codeExecutionExchangeName);
 	}
 
 	@Bean
-	public Binding binding(Queue queue, TopicExchange exchange) {
-		// 교환기와 큐를 연결하여, 라우팅 키가 일치하는 메시지를 큐로 보냅니다.
-		return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+	public Binding codeExecutionBinding() {
+		return BindingBuilder.bind(codeExecutionQueue()).to(codeExecutionExchange()).with(codeExecutionRoutingKey);
+	}
+
+	// 터미널 명령어 큐, 교환기, 바인딩
+	@Bean
+	public Queue terminalCommandQueue() {
+		return new Queue(terminalCommandQueueName, true);
+	}
+
+	@Bean
+	public TopicExchange terminalCommandExchange() {
+		return new TopicExchange(terminalCommandExchangeName);
+	}
+
+	@Bean
+	public Binding terminalCommandBinding() {
+		return BindingBuilder.bind(terminalCommandQueue())
+			.to(terminalCommandExchange())
+			.with(terminalCommandRoutingKey);
 	}
 
 	@Bean
