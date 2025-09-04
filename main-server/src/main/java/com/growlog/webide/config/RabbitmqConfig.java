f64d1d719288a37568756ec917c92a6d8e3eb351
cherish0-0 <jiyu01160@naver.com>
2025-09-04 08:03:44 +0000

    feat: application.yml에 RabbitMQ 설정 값 추가, RabbitmqConfig에 관련 Queue, Exchange, Binding을 Bean으로 등록

--- main-server/src/main/java/com/growlog/webide/config/RabbitmqConfig.java
+++ main-server/src/main/java/com/growlog/webide/config/RabbitmqConfig.java
@@ -4,15 +4,19 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableRabbit
 public class RabbitmqConfig {
 
 	// 코드 실행 큐 관련 설정 값
@@ -30,6 +34,34 @@ public class RabbitmqConfig {
 	private String terminalCommandQueueName;
 	@Value("${terminal-command.rabbitmq.routing.key}")
 	private String terminalCommandRoutingKey;
+
+	// 컨테이너 할당 큐 관련 설정 값
+	@Value("${container-acquire.rabbitmq.exchange.name}")
+	private String containerAcquireExchangeName;
+	@Value("${container-acquire.rabbitmq.queue.name}")
+	private String containerAcquireQueueName;
+	@Value("${container-acquire.rabbitmq.routing.key}")
+	private String containerAcquireRoutingKey;
+
+	// 컨테이너 할당 응답 큐 관련 설정 값
+	@Value("${container-response.rabbitmq.exchange.name}")
+	private String containerResponseExchangeName;
+	@Value("${container-response.rabbitmq.queue.name.acquire-success}")
+	private String containerAcquireSuccessQueueName;
+	@Value("${container-response.rabbitmq.queue.name.acquire-failure}")
+	private String containerAcquireFailureQueueName;
+	@Value("${container-response.rabbitmq.routing.key.acquire-success}")
+	private String acquireSuccessRoutingKey;
+	@Value("${container-response.rabbitmq.routing.key.acquire-failure}")
+	private String acquireFailureRoutingKey;
+
+	// 컨테이너 정리 큐 관련 설정 값
+	@Value("${container-cleanup.rabbitmq.exchange.name}")
+	private String containerCleanupExchangeName;
+	@Value("${container-cleanup.rabbitmq.queue.name}")
+	private String containerCleanupQueueName;
+	@Value("${container-cleanup.rabbitmq.routing.key}")
+	private String containerCleanupRoutingKey;
 
 	// 코드 실행 큐, 교환기, 바인딩
 	@Bean
@@ -65,6 +97,71 @@ public class RabbitmqConfig {
 			.with(terminalCommandRoutingKey);
 	}
 
+	// 컨테이너 할당 큐, 교환기, 바인딩
+	@Bean
+	public Queue containerAcquireQueue() {
+		return new Queue(containerAcquireQueueName, true);
+	}
+
+	@Bean
+	public TopicExchange containerAcquireExchange() {
+		return new TopicExchange(containerAcquireExchangeName);
+	}
+
+	@Bean
+	public Binding containerAcquireBinding() {
+		return BindingBuilder.bind(containerAcquireQueue())
+			.to(containerAcquireExchange())
+			.with(containerAcquireRoutingKey);
+	}
+
+	// 컨테이너 할당 응답 큐, 교환기, 바인딩
+	@Bean
+	public Queue containerAcquireSuccessQueue() {
+		return new Queue(containerAcquireSuccessQueueName, true);
+	}
+
+	@Bean Queue containerAcquireFailureQueue() {
+		return new Queue(containerAcquireFailureQueueName, true);
+	}
+
+	@Bean
+	public TopicExchange containerResponseExchange() {
+		return new TopicExchange(containerResponseExchangeName);
+	}
+
+	@Bean
+	public Binding acquireSuccessBinding() {
+		return BindingBuilder.bind(containerAcquireSuccessQueue())
+			.to(containerResponseExchange())
+			.with(acquireSuccessRoutingKey);
+	}
+
+	@Bean
+	public Binding acquireFailureBinding() {
+		return BindingBuilder.bind(containerAcquireFailureQueue())
+			.to(containerResponseExchange())
+			.with(acquireFailureRoutingKey);
+	}
+
+	// 컨테이너 정리 큐, 교환기, 바인딩
+	@Bean
+	public Queue containerCleanupQueue() {
+		return new Queue(containerCleanupQueueName, true);
+	}
+
+	@Bean
+	public TopicExchange containerCleanupExchange() {
+		return new TopicExchange(containerCleanupExchangeName);
+	}
+
+	@Bean
+	public Binding containerCleanupBinding() {
+		return BindingBuilder.bind(containerCleanupQueue())
+			.to(containerCleanupExchange())
+			.with(containerCleanupRoutingKey);
+	}
+
 	@Bean
 	public MessageConverter jsonMessageConverter() {
 		// DTO 객체를 JSON으로 직렬화/역직렬화하기 위한 메시지 컨버터입니다.
@@ -78,5 +175,17 @@ public class RabbitmqConfig {
 		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
 		rabbitTemplate.setMessageConverter(messageConverter);
 		return rabbitTemplate;
+	}
+
+	@Bean
+	public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+		ConnectionFactory connectionFactory,
+		SimpleRabbitListenerContainerFactoryConfigurer configurer,
+		MessageConverter messageConverter) {
+
+		SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+		configurer.configure(factory, connectionFactory);
+		factory.setMessageConverter(messageConverter);
+		return factory;
 	}
 }
