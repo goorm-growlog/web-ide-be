1a4ec4f05a652d9eef1ac5a097f5e45e17cb0b3f
yuj118 <yujin9600@naver.com>
2025-09-07 16:36:29 +0900

    feat: 컨테이너 작동 방식 변경으로 인한 dto 변경

--- worker-server/src/main/java/com/growlog/webide/workers/execution/dto/CodeExecutionRequestDto.java
+++ worker-server/src/main/java/com/growlog/webide/workers/execution/dto/CodeExecutionRequestDto.java
@@ -9,6 +9,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class CodeExecutionRequestDto {
 	private Long projectId;
+	private String executionLogId; // 일회성 실행(코드 실행)을 식별하기 위한 ID
 	private Long userId;
 	private String language;
 	private String filePath;
