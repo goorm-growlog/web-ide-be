1fa535043ad602186e955ed5bf2007765611d8ab
yuj118 <yujin9600@naver.com>
2025-09-07 16:35:58 +0900

    feat: 터미널 실행 시 컨테이너 아이디를 요청 파라미터에 추가

--- worker-server/src/main/java/com/growlog/webide/workers/execution/dto/TerminalCommandRequestDto.java
+++ worker-server/src/main/java/com/growlog/webide/workers/execution/dto/TerminalCommandRequestDto.java
@@ -10,6 +10,7 @@ import lombok.Setter;
 public class TerminalCommandRequestDto {
 	private Long projectId;     // 프로젝트 식별자
 	private Long userId;        // 요청한 사용자를 식별
+	private String containerId;
 	private String command;     // 실행할 명령어 (e.g., "ls -al", "git status")
 	private String dockerImage;
 }
